26039be591dcc5c20c2dafc6e101b98fb19b995a
Chubbard022 <chubbard022@gmail.com>
2019-05-08 15:20:20 -0700

    started to create react application:

--- api/server.js
+++ api/server.js
@@ -13,6 +13,7 @@ server.use(cors())
 
 server.use("/api/auth",authRouter)
 server.use("/api/users",userRouter)
+server.use(cors({credentials:true, origin: "http://localhost:5000"}))
 
 server.get("/",(req,res)=>{ res.send("Server working") })
 
