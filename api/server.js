23d32b963ab66a7ba0f1dc12609d3a0b471a3fa9
Chubbard022 <chubbard022@gmail.com>
2019-05-08 13:01:18 -0700

    auth routes finished, server.js routed up

--- api/server.js
+++ api/server.js
@@ -2,14 +2,18 @@ const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
 
+const authRouter = require("../auth/auth-router")
+const userRouter = require("../users/user-router")
+
 const server = express()
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get("/",(req,res)=>{
-    res.send("Server working")
-})
+server.use("/api/auth",authRouter)
+server.use("/api/users",userRouter)
+
+server.get("/",(req,res)=>{ res.send("Server working") })
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,18 +3,50 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const Users = require("../users/user-model")
-const secret = require("../config/secrets")
+const secrets = require("../config/secrets")
 
 router.post("/register",(req,res)=>{
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password,10)
 
+    user.password = hash;
+
+    Users.add(user)
+        .then(users=>{
+            res.status(201).json(users)
+        })
+        .catch(err=>{
+            res.status(500).json({errorMessage:"registration failure"})
+        })
 })
 
 router.post("/login",(req,res)=>{
-
+    let {username, password} = req.body;
+    Users.findBy({ username })
+        .first()
+        .then(user=>{
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = generateToken(user)
+                res.status(200).json({ successMessage:`Welcome back ${user.username}`, token })
+            }else{
+                res.status(401).json({errorMessage: "incorrect username or password"})
+            }
+        })
+    .catch(err=>{
+        res.status(500).json({errorMessage: "error occured when logging in"})
+    })
 })
 
 function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    }
+    const options = {
+        expiresIn: '1h'
+    }
 
+    return jwt.sign(payload, secrets.jwtSecret , options)
 }
 
-module.exports = router;
+module.exports = router;xs
