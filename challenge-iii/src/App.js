4b9c03ccadce206575ac9354521798dc8c69421c
Chubbard022 <chubbard022@gmail.com>
2019-05-09 11:32:04 -0700

    app.js importing and index.js importing complete. BrowserRouter set up within index.js

--- challenge-iii/src/App.js
+++ challenge-iii/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import axios from 'axios'
+import { Route,NavLink, withRouter } from "react-router-dom"
+
+//import Login from " "
+//import Users from " "
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     hello from react-app
-    </div>
-  );
+const URL = "http://localhost:5000"
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      users:[]
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get(`URL`)
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+      Welcome to the React App
+      </div>
+    )
+  }
 }
 
 export default App;
