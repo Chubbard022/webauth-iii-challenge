5a8f8e8eb025a0c2fd2575de5c915e8dee20ceed
Chubbard022 <chubbard022@gmail.com>
2019-05-09 12:12:03 -0700

    Login for react application fleshed out

--- challenge-iii/src/auth/Login.js
+++ challenge-iii/src/auth/Login.js
@@ -3,12 +3,50 @@ import axios from "axios";
 
 class Login extends React.Component {
     state={
-
+        username: "mr",
+        password: "curtis"
     }
+
+    handleChange = event =>{
+        event.preventDefault();
+        const {id,value} = event.target;
+        this.setState({[id]:value})
+    }
+
+    handleSubmit = event =>{
+        event.preventDefault();
+        const URL = "http://localhost:5000";
+
+        axios
+            .post(`${URL}/api/auth/login`,this.state)
+            .then(res=>{
+                localStorage.setItem('jwt', res.data.token)
+                this.props.history.push("/users")
+            })
+            .catch(err=>{console.log("LOGIN ERROR::",err)})
+    }
+
     render(){
         return(
             <div>
-                Hello from Login Component
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username" />
+                    <input 
+                        type="text"
+                        id="username"
+                        onChange={this.handleChange}
+                        value={this.state.username}
+                    />xs
+
+                    <label htmlFor="password" />
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={this.handleSubmit}
+                        value={this.state.password}
+                    />
+                <button type="submit"> Submut</button>
+                </form>
             </div>
         )
     }
